7a0f8d2a4767d423141c11fc5646c5a21f047042
Lionel <939338358@qq.com>
2018-11-03 13:33:45 +0800

    更新

--- image-webapp/src/main/java/com/yht/image/web/PageController.java
+++ image-webapp/src/main/java/com/yht/image/web/PageController.java
@@ -1,5 +1,13 @@
 package com.yht.image.web;
 
+import com.qcloud.cos.auth.COSCredentials;
+
+import javax.management.BadAttributeValueExpException;
+
 public class PageController {
 
+
+    public void home() {
+
+    }
 }
