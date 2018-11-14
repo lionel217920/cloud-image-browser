314961ec0065dd2da49c5feacf8c8842552bf30b
Hualihai <939338358@qq.com>
2018-11-14 21:04:14 +0800

    腾讯COS配置类

---
+++ image-webapp/src/main/java/com/yht/image/config/CosConfig.java
@@ -0,0 +1,63 @@
+package com.yht.image.config;
+
+import com.yht.image.factory.ICloudFactory;
+import com.yht.image.tencent.TencentCosFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "cos")
+public class CosConfig {
+
+    private String accessKey;
+
+    private String secrectKey;
+
+    private String bucketName;
+
+    private String regionName;
+
+    @Bean
+    public ICloudFactory cosFactory() {
+        TencentCosFactory factory = new TencentCosFactory();
+        factory.setAccessKey(accessKey);
+        factory.setSecrectKey(secrectKey);
+        factory.setBucketName(bucketName);
+        factory.setRegionName(regionName);
+        return factory;
+    }
+
+
+    public String getAccessKey() {
+        return accessKey;
+    }
+
+    public void setAccessKey(String accessKey) {
+        this.accessKey = accessKey;
+    }
+
+    public String getSecrectKey() {
+        return secrectKey;
+    }
+
+    public void setSecrectKey(String secrectKey) {
+        this.secrectKey = secrectKey;
+    }
+
+    public String getBucketName() {
+        return bucketName;
+    }
+
+    public void setBucketName(String bucketName) {
+        this.bucketName = bucketName;
+    }
+
+    public String getRegionName() {
+        return regionName;
+    }
+
+    public void setRegionName(String regionName) {
+        this.regionName = regionName;
+    }
+}
