54be1c4a89b9d0ca0d42c3b6a14e685c1e22a532
Hualihai <939338358@qq.com>
2018-11-15 17:13:40 +0800

    测试页面修改

--- image-webapp/src/main/java/com/yht/image/web/PageController.java
+++ image-webapp/src/main/java/com/yht/image/web/PageController.java
@@ -4,12 +4,12 @@ import com.yht.image.ICloud;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.InputStream;
 
 @RestController
 @RequestMapping(value = "home")
@@ -26,12 +26,19 @@ public class PageController {
 
     @GetMapping(value = "/put")
     public String index() throws FileNotFoundException {
-        final String IMAGE_PATH = "/home/lionel/IdeaProjects/cloud-image-browser/image-tool/src/test/resources/images/logo.png";
+        final String IMAGE_PATH = "/home/lionel/IdeaProjects/cloud-image-browser/image-tool/src/test/resources/images/Selection_013.png";
         FileInputStream inputStream = new FileInputStream(IMAGE_PATH);
 
-        String result = cloud.putObject("1haitao.jpg", inputStream);
+        String result = cloud.putObject("selection_014.png", inputStream);
         System.out.println(result);
 
         return "aaa";
+    }
+
+    @RequestMapping(value = "message", method = RequestMethod.GET)
+    public ModelAndView message() {
+        ModelAndView view = new ModelAndView("home");
+        view.addObject("message", "hello");
+        return view;
     }
 }
