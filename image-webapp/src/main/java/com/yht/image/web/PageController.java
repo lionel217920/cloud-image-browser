d58474e0da560f0b5363442341c0dc8f57058d23
Hualihai <939338358@qq.com>
2019-07-10 20:00:36 +0800

    update

--- image-webapp/src/main/java/com/yht/image/web/PageController.java
+++ image-webapp/src/main/java/com/yht/image/web/PageController.java
@@ -11,7 +11,7 @@ public class PageController {
 
     @GetMapping
     public ModelAndView home() {
-        return new ModelAndView("index");
+        return new ModelAndView("upload");
     }
 
 }
